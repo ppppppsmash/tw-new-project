08449e0f9e23e22ba48e0addbef6e7649262aa4e
kurosawa <pppppp.work@gmail.com>
2025-12-13 19:57:50 +0900

    feat: Image slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils/cn";
 import { motion, AnimatePresence } from "motion/react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const ImagesSlider = ({
   images,
